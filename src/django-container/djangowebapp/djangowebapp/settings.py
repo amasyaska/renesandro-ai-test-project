62145e1eb997dfb5e1b28c125626054ef1cb216e
amasyaska <bogdan.kolesnikov12345@gmail.com>
2024-10-16 00:55:53 +0300

    changed Dockerfile to apply migrations

--- src/django-container/djangowebapp/djangowebapp/settings.py
+++ src/django-container/djangowebapp/djangowebapp/settings.py
@@ -15,11 +15,6 @@ from pathlib import Path
 import os
 from datetime import timedelta
 
-# turn off on prod
-from dotenv import load_dotenv
-
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
